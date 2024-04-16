f0b1ebc7b11b0b7a3958796c4a8cd4d871f77c9f
ayuska.upadhyaya <ayuska.upadhyaya@student.nisdtx.org>
2024-04-16 13:39:07 -0500

    restarted

--- main.py
+++ main.py
@@ -2,3 +2,24 @@
 # import & initialize
 import pygame
 pygame.init()
+
+# window setup
+run = True
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Elegant Portfolio')
+box = pygame.Rect((300, 250, 50, 50))
+
+# main loop
+while run:
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (255, 0, 0), box)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
