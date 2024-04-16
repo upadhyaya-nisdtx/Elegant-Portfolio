d8fde22ab7b1a09282641bddc3f40b9e449124f5
ayuska.upadhyaya <ayuska.upadhyaya@student.nisdtx.org>
2024-04-16 14:03:42 -0500

    started set up of first interface

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ pygame.init()
 
 # window setup
 run = True
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 2000
+SCREEN_HEIGHT = 1000
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Elegant Portfolio')
-box = pygame.Rect((300, 250, 50, 50))
+
+# screen booleans
+first_interface_visible = True
+second_interface_visible = False
+edit_interface_visible = False
+
+# first interface buttons
+new_project_button = pygame.Rect((200, 450, 600, 400))
+load_saves_button = pygame.Rect((1200, 450, 600, 400))
 
 # main loop
 while run:
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), box)
+
+    if first_interface_visible:
+        screen.fill((238, 130, 238, 255))
+        pygame.draw.rect(screen, (255, 255, 255, 255), new_project_button)
+        pygame.draw.rect(screen, (255, 255, 255, 255), load_saves_button)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
