e196f27789ec4a81ae0ebdbb5ad0aa0229a4e580
ayuska.upadhyaya <ayuska.upadhyaya@student.nisdtx.org>
2024-04-17 13:52:09 -0500

    Graphics for first interface set up

--- main.py
+++ main.py
@@ -15,17 +15,31 @@ first_interface_visible = True
 second_interface_visible = False
 edit_interface_visible = False
 
-# first interface buttons
+# first interface graphics
+
 new_project_button = pygame.Rect((200, 450, 600, 400))
+new_project_title = pygame.font.SysFont("Times New Roman", 100, italic=True)
+new_project_txtsurf = new_project_title.render("New Project", True, (200, 200, 255, 255))
+
 load_saves_button = pygame.Rect((1200, 450, 600, 400))
+load_saves_title = pygame.font.SysFont("Times New Roman", 100, italic=True)
+load_saves_txtsurf = load_saves_title.render("Load Saves", True, (200, 200, 255, 255))
+
+title = pygame.font.SysFont("Times New Roman", 150, italic=True)
+title_txtsurf = title.render("Elegant Portfolio", True, (255,255,255))
 
 # main loop
 while run:
 
     if first_interface_visible:
-        screen.fill((238, 130, 238, 255))
-        pygame.draw.rect(screen, (255, 255, 255, 255), new_project_button)
-        pygame.draw.rect(screen, (255, 255, 255, 255), load_saves_button)
+        screen.fill((200, 200, 255, 255))
+        pygame.draw.rect(screen, (255, 255, 255, 255), new_project_button, 400, 20)
+        screen.blit(new_project_txtsurf, (500 - new_project_txtsurf.get_width() // 2,
+                                          635 - new_project_txtsurf.get_height() // 2))
+        pygame.draw.rect(screen, (255, 255, 255, 255), load_saves_button, 400, 20)
+        screen.blit(load_saves_txtsurf, (1500 - load_saves_txtsurf.get_width() // 2,
+                                          635 - load_saves_txtsurf.get_height() // 2))
+        screen.blit(title_txtsurf, (1000 - title_txtsurf.get_width() // 2, 250 - title_txtsurf.get_height() // 2))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
