dd16cfe2ca3201e321723f0c40f039482ee07667
ayuska.upadhyaya <ayuska.upadhyaya@student.nisdtx.org>
2024-04-24 13:53:37 -0500

    functions

--- main.py
+++ main.py
@@ -34,47 +34,55 @@ exit_button = pygame.transform.scale(exit_button, (210, 180))
 new_project_button = pygame.Rect((200, 450, 600, 400))
 new_project_title = pygame.font.SysFont("Times New Roman", 100)
 new_project_txtsurf = new_project_title.render("New Project", True, (200, 200, 255, 255))
-
 load_saves_button = pygame.Rect((1200, 450, 600, 400))
 load_saves_title = pygame.font.SysFont("Times New Roman", 100)
 load_saves_txtsurf = load_saves_title.render("Load Saves", True, (200, 200, 255, 255))
+def first_int_graphics():
+    screen.fill((200, 200, 255, 255))
+    pygame.draw.rect(screen, (255, 255, 255, 255), new_project_button, 400, 20)
+    screen.blit(new_project_txtsurf, (500 - new_project_txtsurf.get_width() // 2,
+                                      635 - new_project_txtsurf.get_height() // 2))
+    pygame.draw.rect(screen, (255, 255, 255, 255), load_saves_button, 400, 20)
+    screen.blit(load_saves_txtsurf, (1500 - load_saves_txtsurf.get_width() // 2,
+                                     635 - load_saves_txtsurf.get_height() // 2))
+    screen.blit(title_txtsurf, (1000 - title_txtsurf.get_width() // 2, 250 - title_txtsurf.get_height() // 2))
+    screen.blit(logo, (1750, 0))
 
 # second interface graphics
 scroll_surface = pygame.Surface((1500, 700))
 
+def second_int_graphics():
+    screen.fill((200, 200, 255, 255))
+    screen.blit(logo, (1750, 0))
+    screen.blit(title_txtsurf, (600 - title_txtsurf.get_width() // 2, 100 - title_txtsurf.get_height() // 2))
+    scroll_surface.fill((255, 255, 255, 255))
+    screen.blit(exit_button, (1550, 0))
+    screen.blit(scroll_surface, (200, 200))
+
 # edit interface graphics
 edit_surface = pygame.Surface((500, 1000))
+
+def edit_int_graphics():
+    screen.fill((255, 255, 255, 255))
+    edit_surface.fill((200, 200, 255, 255))
+    screen.blit(edit_surface, (1500, 0))
+    edit_surface.blit(exit_button, (1700, 100))
+    screen.blit(exit_button, (1550, 0))
 
 # main loop
 while run:
 
     # run while first interface open
     if first_interface_visible:
-        screen.fill((200, 200, 255, 255))
-        pygame.draw.rect(screen, (255, 255, 255, 255), new_project_button, 400, 20)
-        screen.blit(new_project_txtsurf, (500 - new_project_txtsurf.get_width() // 2,
-                                          635 - new_project_txtsurf.get_height() // 2))
-        pygame.draw.rect(screen, (255, 255, 255, 255), load_saves_button, 400, 20)
-        screen.blit(load_saves_txtsurf, (1500 - load_saves_txtsurf.get_width() // 2,
-                                         635 - load_saves_txtsurf.get_height() // 2))
-        screen.blit(title_txtsurf, (1000 - title_txtsurf.get_width() // 2, 250 - title_txtsurf.get_height() // 2))
-        screen.blit(logo, (1750, 0))
+        first_int_graphics()
 
     # run while second interface open
     elif second_interface_visible:
-        screen.fill((200, 200, 255, 255))
-        screen.blit(logo, (1750, 0))
-        screen.blit(title_txtsurf, (600 - title_txtsurf.get_width() // 2, 100 - title_txtsurf.get_height() // 2))
-        scroll_surface.fill((255, 255, 255, 255))
-        screen.blit(scroll_surface, (200, 200))
-        screen.blit(exit_button, (1550, 0))
+        second_int_graphics()
 
     # run while edit interface open
     elif edit_interface_visible:
-        screen.fill((255, 255, 255, 255))
-        edit_surface.fill((200, 200, 255, 255))
-        screen.blit(edit_surface, (1500, 0))
-        edit_surface.blit(exit_button, (1700, 100))
+        edit_int_graphics()
 
     # cursor movement
     pos = pygame.mouse.get_pos()
@@ -88,12 +96,22 @@ while run:
             user_cursor = pygame.image.load("custom images/clickarrow.png")
             user_cursor = pygame.transform.scale(user_cursor, (180, 120))
             user_cursor = pygame.transform.rotate(user_cursor, 45)
-            if load_saves_button.collidepoint(pos):
-                first_interface_visible = False
-                second_interface_visible = True
-            elif new_project_button.collidepoint(pos):
-                first_interface_visible = False
-                edit_interface_visible = True
+            if first_interface_visible:
+                if load_saves_button.collidepoint(pos):
+                    first_interface_visible = False
+                    second_interface_visible = True
+                elif new_project_button.collidepoint(pos):
+                    first_interface_visible = False
+                    edit_interface_visible = True
+            elif second_interface_visible:
+                if exit_button.get_rect().collidepoint(pos):
+                    first_interface_visible = True
+                    second_interface_visible = False
+            elif edit_interface_visible:
+                if exit_button.get_rect().collidepoint(pos):
+                    first_interface_visible = True
+                    edit_interface_visible = False
+
         elif event.type == pygame.MOUSEBUTTONUP:
             user_cursor = pygame.image.load("custom images/arrow.png")
             user_cursor = pygame.transform.scale(user_cursor, (180, 120))
